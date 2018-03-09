37e87012dfb0e3c5b41cf3277d4e0cf450395036
zhengsc <273267898@qq.com>
2018-03-09 15:02:23 +0800

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 let path = require('path')
 let webpack = require('webpack')
 let HTMLPlugin = require('html-webpack-plugin')
+let ExtractText = require('extract-text-webpack-plugin')
 
 let isDev = process.env.NODE_ENV === 'development' 
 
 let config = {
     target: 'web',
+    mode: process.env.NODE_ENV,
     entry: path.join(__dirname, 'src/index.js'),
     output: {
         filename: 'bould.js',
@@ -19,11 +21,8 @@ let config = {
                 use: 'vue-loader'
             },
             {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
+                test: /\.jsx$/,
+                use: 'babel-loader'
             },
             {
                 test: /\.(jpg|png|jpeg|gif|svg)$/,
@@ -54,6 +53,16 @@ let config = {
 if(isDev) {
     config.devtool = '#cheap-module-eval-source-map'
 
+    config.module.rules.push( {
+        test: /\.css$/,
+        use: [
+            'style-loader',
+            'css-loader',
+            'postcss-loader',
+            'sass-loader'
+        ]
+    })
+
     config.devServer = {
         port: 8000,
         host: '0.0.0.0',
@@ -67,6 +76,21 @@ if(isDev) {
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
     )
+} else {
+
+    config.module.rules.push({
+        test: /\.css$/,
+        use: ExtractText.extract({
+            fallback: 'style-loader',
+            use: [
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+        })
+    })
+
+    config.plugins.push(new ExtractText('style-[chunkhash:8].css'))
 }
 
 module.exports = config
